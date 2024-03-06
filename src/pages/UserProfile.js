76e373d87af616728008a1f101326829934f218e
Yonathan Beltran Romero <ybeltran@freightclaims.com>
2024-03-05 23:39:05 -0500

    Fin de registro de usuario

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -2,8 +2,11 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import NewsLetter from "../elements/NewsLetter";
 import PageTitle from "../elements/PageTitle";
-import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
+
 const UserProfile = () => {
 
   return (
@@ -31,16 +34,41 @@ const UserProfile = () => {
                     <input type="text" className="form-control" placeholder="" aria-label="apellidos" aria-describedby="apellidos" />
                   </div>
 
-                  
+
                   <div className="input-group mb-3  input-line">
                     <span className="input-group-text" id="email">Email</span>
                     <input type="text" className="form-control" placeholder="" aria-label="email" aria-describedby="email" />
+
+
+
                   </div>
 
                   <div className="input-group mb-3  input-line">
-                    <span className="input-group-text" id="basic-addon1">Telefono</span>
-                    <input type="text" className="form-control" placeholder="" aria-label="telefono" aria-describedby="basic-addon1" />
+                    <span className="input-group-text" id="telefono">Telefono</span>
+                    <input type="text" className="form-control" placeholder="" aria-label="telefono" aria-describedby="telefono" />
                   </div>
+                  <div className="input-group mb-3  input-line">
+                    <span className="input-group-text" id="fechanacimiento">Fecha de nacimiento</span>
+                    <input type="date" className="form-control" placeholder="" aria-label="fechanacimiento" aria-describedby="fechanacimiento" />
+                  </div>
+
+                  <div className="input-group mb-3  input-line">
+                    <span className="input-group-text" id="genero">Genero</span>
+                    <select class="form-control" aria-label="genero">
+                      <option value="1">Masculino</option>
+                      <option value="2">Femenino</option>
+                    </select>
+                  </div>
+                  <div className="input-group mb-3  input-line">
+                    <span className="input-group-text" id="edad">Edad</span>
+                    <input type="number" className="form-control" placeholder="" aria-label="edad" aria-describedby="edad" />
+                  </div>
+                  <div className="input-group mb-3  input-line">
+                    <span className="input-group-text" id="peso">Peso</span>
+                    <input type="number" className="form-control" placeholder="" aria-label="peso" aria-describedby="peso" />
+                  </div>
+              
+
 
                   <div className="row d-flex justify-content-around" >
                     <button
@@ -65,19 +93,155 @@ const UserProfile = () => {
                 <div className='col-md-6 col-lg-6 col-sm-12'>
 
                   <Tabs
-                    defaultActiveKey="profile"
+                    defaultActiveKey="0
+                    "
                     id="uncontrolled-tab-example"
                     className="mb-3"
                   >
+                    <Tab eventKey="0" title="Perfil Geografico">
+                    <div className='row'>
+                        <div className='col-md-12 col-lg-12 col-sm-12'>
+                        <div className="input-group mb-3  input-line">
+                    <span className="input-group-text" id="pais">Pais</span>
+                    <select class="form-control" aria-label="pais">
+                      <option value="1">Colombia</option>
+                      <option value="2">Ecuador</option>
+                    </select>
+                  </div>
+
+                  <div className="input-group mb-3  input-line">
+                    <span className="input-group-text" id="Estado">Estado</span>
+                    <select class="form-control" aria-label="Estado">
+                      <option value="1">Antioquia</option>
+                      <option value="2">Bogota</option>
+                    </select>
+                  </div>
+
+                  <div className="input-group mb-3  input-line">
+                    <span className="input-group-text" id="Ciudad">Ciudad</span>
+                     <select class="form-control" aria-label="Ciudad">
+                      <option value="1">Medellin</option>
+                      <option value="2">Bello</option>
+                    </select>
+                  </div>
+                          </div>
+                          </div>
+                    </Tab>
+
                     <Tab eventKey="1" title="Perfil alimenticio">
-                      Tab content for Home
+                      <div className='row'>
+                        <div className='col-md-12 col-lg-12 col-sm-12'>
+                          <div className="input-group mb-3  input-line">
+                            <span className="input-group-text" id="verdurasSemana">Cuantas veces consumes verduras a la semana?</span>
+                            <input type="number" className="form-control" placeholder="" aria-label="verdurasSemana" aria-describedby="verdurasSemana" />
+                          </div>
+                          <div className="input-group mb-3  input-line">
+                            <span className="input-group-text" id="comidaRapidaSemana">Cuantas veces consumes comida rapida?</span>
+                            <input type="number" className="form-control" placeholder="" aria-label="comidaRapidaSemana" aria-describedby="comidaRapidaSemana" />
+                          </div>
+                          <div className="input-group mb-3  input-line">
+                            <span className="input-group-text" id="vasosAguaDia">Cuantas vasos de agua consumes al dia?</span>
+                            <input type="number" className="form-control" placeholder="" aria-label="vasosAguaDia" aria-describedby="vasosAguaDia" />
+                          </div>
+                          <div className="input-group mb-3  input-line">
+                            <span className="input-group-text" id="caloriasDias">Sabes cuantas calorias consumes al dia</span>
+                            <Form.Check // prettier-ignore
+                              type="switch"
+                              id="custom-switch"
+                            />
+
+
+                          </div>
+
+
+
+                        </div>
+
+                      </div>
                     </Tab>
                     <Tab eventKey="2" title="Perfil deportivo">
-                      Tab content for Profile
+                      <div className='row'>
+                        <div className='col-md-12 col-lg-12 col-sm-12'>
+                          <div className="input-group mb-3  input-line">
+                            <span className="input-group-text" id="ejercicioSemana">Cuantas veces haces ejercicio por semana?</span>
+                            <input type="number" className="form-control" placeholder="" aria-label="ejercicioSemana" aria-describedby="ejercicioSemana" />
+                          </div>
+                          <div className="input-group mb-3  input-line">
+                            <span className="input-group-text" id="tienesLesiones">Tienes lesiones</span>
+                            <Form.Check // prettier-ignore
+                              type="switch"
+                              id="custom-switch"
+                              className='mt-2 secondary'
+                              aria-label="tienesLesiones" aria-describedby="tienesLesiones"
+                            />
+                          </div>
+                          <div className="input-group mb-3  input-line">
+                            <span className="input-group-text" id="lessiones">Que lessiones tienes?</span>
+                            <textarea type="Area" className="form-control" placeholder="" aria-label="lessiones" aria-describedby="lessiones" />
+                          </div>
+                          <div className="input-group mb-3  p-3 input-line">
+                            <span className="input-group-text" id="lessiones">Actividades deportivas favoritas</span>
+                            {['checkbox',].map((type) => (
+                              <div key={`inline-${type}`} className="mb-3">
+                                <Form.Check
+                                  inline
+                                  variant="dark"
+                                  label="Futbol"
+                                  className='mt-3'
+                                  name="group1"
+                                  type={type}
+                                  id={`inline-${type}-1`}
+                                />
+                                <Form.Check
+                                  inline
+                                  variant="dark"
+                                  label="Basket"
+                                  className='mt-3'
+                                  name="group1"
+                                  type={type}
+                                  id={`inline-${type}-2`}
+                                />
+                                <Form.Check
+                                  inline
+                                  variant="dark"
+                                  label="Ciclismo"
+                                  className='mt-3'
+                                  name="group1"
+                                  type={type}
+                                  id={`inline-${type}-2`}
+                                />
+                                <Form.Check
+                                  inline
+                                  label="Zumba"
+                                  variant="dark"
+                                  className='mt-3'
+                                  name="group1"
+                                  type={type}
+                                  id={`inline-${type}-2`}
+                                />
+                                <Form.Check
+                                  inline
+                                  variant="dark"
+                                  label="Correr"
+                                  className='mt-3'
+                                  name="group1"
+                                  type={type}
+                                  id={`inline-${type}-2`}
+                                />
+                              </div>
+
+                            ))}
+                          </div>
+
+
+
+                        </div>
+
+                      </div>
                     </Tab>
-                    <Tab eventKey="3" title="Contact">
-                          Mi info
-                    </Tab>
+                    {/* <Tab eventKey="3" title="Contact">
+                      Mi info
+                    </Tab> */}
                   </Tabs>
 
                 </div>
