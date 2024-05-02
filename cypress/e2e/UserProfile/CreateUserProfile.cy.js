86d226c2ecc81129c19ff55f7c50bc68c8c6510a
ELISA ANGULO <elisaangulovanegas@gmail.com>
2024-05-01 19:49:00 -0500

    Se crea prueba cypress de inscripcion de planes alimenticios

--- cypress/e2e/UserProfile/CreateUserProfile.cy.js
+++ cypress/e2e/UserProfile/CreateUserProfile.cy.js
@@ -15,7 +15,7 @@ describe("Creates user  profile", () => {
       pageObj.waiting(3500);
       pageObj.generateAction("login", "link inicio session", "click");
       pageObj.waiting(3500);
-      pageObj.generateActionTypeValue("login", "txtEmail", "nathanbelt23@gmail.com");
+      pageObj.generateActionTypeValue("login", "txtEmail", "eli@gmail.com");
       pageObj.waiting(1000);
       pageObj.generateActionTypeValue("login", "txtPassword", "Hope2028*");
       pageObj.waiting(1000);
